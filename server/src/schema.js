281bb3e6f98679a9f75f42dda9f2189f12cfa373
michaelballos <mdb3091207@gmai.com>
2022-04-29 14:13:06 -0500

    Add
    Query

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,6 +1,10 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
+  type Query {
+    tracksForHome: [Track!]!
+  }
+
 "A track is a group of modules that teaches about specific topic"
   type Track {
     id: ID!
