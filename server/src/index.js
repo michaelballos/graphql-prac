0a291a90de3da8d91cb8e9c1d72746ca4953b2d0
michaelballos <mdb3091207@gmai.com>
2022-04-29 14:26:45 -0500

    Add
    Apollo server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,8 @@
-//TODO
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({typeDefs});
+
+server.listen().then(() => {
+  console.log('Server is running on http://localhost:4000');
+})
